0e02cc1cd64d372b2b16dd8a35d93fabb7d8bfac
Elisa Chang <Elisa.Chang@zyxel.com.tw>
2024-01-28 15:17:52 +0800

    [feat] router and pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
 import './App.css'
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import {HomePage, LoginPage, SignUpPage } from './pages';
 
 function App() {
   return (
     <>
-      <div>
-        test
+      <div className="container">
+        <BrowserRouter>
+        <Routes>
+          <Route path="login" element={<LoginPage />} />
+          <Route path="signup" element={<SignUpPage />} />
+          <Route path="*" element={<HomePage />} />
+        </Routes>
+      </BrowserRouter>
       </div>
     </>
   )
--- src/utils/query.tsx
+++ src/utils/query.tsx
@@ -2,3 +2,14 @@ import useSWR from 'swr';
 import axios from 'axios';
 
 const getFetcher = (url: string) => axios.get(url).then(res => res.data);
+// const postFetcher = (url: string) => axios.get(url).then(res => res.data);
+
+// export function useUser (id) {
+//   const { data, error, isLoading } = useSWR(`/api/user/${id}`, getFetcher)
+ 
+//   return {
+//     user: data,
+//     isLoading,
+//     isError: error
+//   }
+// }
