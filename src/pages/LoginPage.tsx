7d344cc080e5ed93a15d9b14934bd34d3eb79f73
Elisa Chang <Elisa.Chang@zyxel.com.tw>
2024-01-30 01:29:17 +0800

    [feat] update layout & css

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import { useForm, SubmitHandler } from 'react-hook-form';
+import { Header } from '../components';
 import { useAuth } from '../utils/authContext';
 import { LoginInput } from '../utils/apiSchema'
 
@@ -18,14 +19,23 @@ export default function LoginPage() {
     };
     
     return (<>
-        <h1>login</h1>
-        <form onSubmit={handleSubmit(onSubmit)}>
-            <input type="text" placeholder="Email" {...register("email", {required: true})} />
-            <input type="password" placeholder="Password" {...register("password", { required: true, minLength: 8 })} onChange={() => setErrMsg('')} />
-            {errors.password && errors.password.type === "minLength" && (<span>密碼至少 8 碼以上</span>)}
-            {errMsg && (<span>{errMsg}</span>)}
+        <Header />
+        <div>
+            <p>享樂酒店，誠摯歡迎</p>
+            <h1>立即開始旅程</h1>
+            <form onSubmit={handleSubmit(onSubmit)}>
+                <label htmlFor="email">電子信箱</label>
+                <input id="email" type="text" placeholder="Email" {...register("email", { required: true })} />
+                <label htmlFor="password">密碼</label>
+                <input id="password" type="password" placeholder="Password" {...register("password", { required: true, minLength: 8 })} onChange={() => setErrMsg('')} />
+                {/*TODO: Add more reminder msg  */}
+                {errors.password && errors.password.type === "minLength" && (<span>密碼至少 8 碼以上</span>)}
+                {errMsg && (<span>{errMsg}</span>)}
 
-            <input type="submit" />
-        </form>
+                <button type="submit">會員登入</button>
+                <p>沒有會員嗎？</p>
+                <a href={`/signup`}>前往註冊</a>
+            </form>
+        </div>
     </>);
 }
--- src/pages/SignUpPage.tsx
+++ src/pages/SignUpPage.tsx
@@ -1,5 +1,8 @@
+import { Header } from '../components';
+
 export default function SignUpPage() {
     return (<>
+        <Header />
         <h1>signup</h1>
     </>);
 }
